89c27401acc30e5ff4ecaf9c8dd6cb5941494e85
Kaden Baxter <kadebaxter@gmail.com>
2024-05-18 01:18:19 -0600

    Added most the css

---
+++ client/domain.js
@@ -0,0 +1,13 @@
+//Need map and filter array functions
+//Manipulate the DOM
+//Adding event listeners
+//Javascript across files
+//Structured code
+//Async await and promises
+//Network requests, API pulls
+//Read querystring for some reason
+//local storage
+
+//other stuff: filter bar
+//Drag & drop (maybe)
+//Shared page layout between everything. 
